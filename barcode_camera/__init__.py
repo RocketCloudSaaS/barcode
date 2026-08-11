076d695c9345509c702c063ba6d19274b687b3bc
Zuzanna Elzbieta Szalaty Szalaty <szalaty.susana@gmail.com>
2026-08-11 14:11:26 +0100

    [ADD] barcode_camera: scan with the device camera
    Add an optional camera button (FAB) that scans using the device camera
    through the web barcode dialog, feeding the same scan pipeline.
    
    - depends only on barcode_scanner

---
+++ barcode_camera/__init__.py
@@ -0,0 +1,2 @@
+# barcode_camera has no Python models; it registers a camera scan-source
+# widget into the barcode_scanner framework from the frontend assets.
---
+++ barcode_camera/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Barcode Camera",
+    "version": "18.0.1.0.0",
+    "category": "Inventory/Logistics",
+    "summary": "Mobile camera barcode scanner (EAN13) for the Barcode suite",
+    "author": "Binhex, Odoo Community Association (OCA)",
+    "website": "https://github.com/RocketCloudSaaS/barcode",
+    "maintainers": ["antoniodavid", "szalatyzuzanna"],
+    "license": "AGPL-3",
+    "depends": [
+        "barcode_scanner",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "barcode_camera/static/src/scss/barcode_camera.scss",
+            "barcode_camera/static/src/xml/camera_fab.xml",
+            "barcode_camera/static/src/js/camera_fab.js",
+        ],
+    },
+    "installable": True,
+    "auto_install": False,
+}
