3506818757607971504d3415694d667f3cd8d182
Zuzanna Elzbieta Szalaty Szalaty <szalaty.susana@gmail.com>
2026-09-09 11:30:28 +0100

    [FIX] barcode_stock: keep the .esm suffix on relative component imports

--- barcode_stock/static/src/js/components/picking_move_list.esm.js
+++ barcode_stock/static/src/js/components/picking_move_list.esm.js
@@ -1,5 +1,5 @@
 import {Component} from "@odoo/owl";
-import {PickingMoveCard} from "./picking_move_card";
+import {PickingMoveCard} from "./picking_move_card.esm";
 import {isMovePending} from "@barcode_stock/js/utils/move_progress.esm";
 
 export class PickingMoveList extends Component {
